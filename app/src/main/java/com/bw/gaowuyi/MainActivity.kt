9b9bcb34b582bdfefcb0f494a9a519298205ee57
bjam <893739562@qq.com>
2024-01-08 14:03:04 +0800

    561951

---
+++ app/src/main/java/com/bw/gaowuyi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bw.gaowuyi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
